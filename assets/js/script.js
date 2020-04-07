177138d8a22a7a4a2fff9243ebf7cfda6b67eaac
Terry Thompson <terrylthompsonintx@gmail.com>
2020-04-07 15:35:30 -0500

    added type to output

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,18 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    var taskInfoEl = document.createElement("div");
+    
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    //listItemEl.textContent = taskNameInput;
+    //listItemEl.textContent = taskTypeInput;
+    listItemEl.appendChild(taskInfoEl);
+    console.log(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
   }
   formEl.addEventListener("submit", createTaskHandler);
